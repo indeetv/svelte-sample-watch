7ca409ebadca8b014c9a34625ad44a9d75964e2d
Jesvin <jesvin@indee.tv>
2024-10-14 15:47:09 +0530

    Standardized logout button

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -28,22 +28,24 @@
 			<button
 				on:click={handleLogout}
 				type="button"
-				class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				class="text-white w-24 bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus-visible:outline-indigo-600 font-medium rounded-lg text-sm px-4 py-2 text-center"
 			>
 				{#if $loginStoreSnapshot.logoutApiCalled}
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-						><path
-							fill="currentColor"
-							d="M12,23a9.63,9.63,0,0,1-8-9.5,9.51,9.51,0,0,1,6.79-9.1A1.66,1.66,0,0,0,12,2.81h0a1.67,1.67,0,0,0-1.94-1.64A11,11,0,0,0,12,23Z"
-							><animateTransform
-								attributeName="transform"
-								dur="0.75s"
-								repeatCount="indefinite"
-								type="rotate"
-								values="0 12 12;360 12 12"
-							/></path
-						></svg
-					>
+					<div class="w-full items-center flex justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+							><path
+								fill="currentColor"
+								d="M12,23a9.63,9.63,0,0,1-8-9.5,9.51,9.51,0,0,1,6.79-9.1A1.66,1.66,0,0,0,12,2.81h0a1.67,1.67,0,0,0-1.94-1.64A11,11,0,0,0,12,23Z"
+								><animateTransform
+									attributeName="transform"
+									dur="0.75s"
+									repeatCount="indefinite"
+									type="rotate"
+									values="0 12 12;360 12 12"
+								/></path
+							></svg
+						>
+					</div>
 				{:else}
 					Log out
 				{/if}
